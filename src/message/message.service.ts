b4548a6e09bb802702f38f4aa76a7dd6967da751
Solomon Olatunji <realsolomon01@outlook.com>
2023-07-13 08:23:19 +0100

    updating

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -5,8 +5,8 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import { Message } from "@app/schemas";
-import { User } from "@app/schemas"
+import { Message } from '@app/schemas';
+import { User } from '@app/schemas';
 import { Query } from 'express-serve-static-core';
 
 @Injectable()
@@ -17,9 +17,35 @@ export class MessageService {
     ) { }
 
     async create(message: Message, user: User): Promise<Message> {
+        if (!message.message) {
+            throw new BadRequestException('Message is required');
+        }
+
         const data = Object.assign(message, { user: user._id });
 
         const savedMessage = await this.messageModel.create(data);
         return savedMessage;
     }
+
+    async messages(query: Query, user: User): Promise<Message[]> {
+        const resPerPage = 2;
+        const currentPage = Number(query.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        const keyword = query.keyword
+            ? {
+                text: {
+                    $regex: query.keyword,
+                    $options: 'i',
+                },
+            }
+            : {};
+
+        const messages = await this.messageModel
+            .find({ user: user._id, ...keyword })
+            .limit(resPerPage)
+            .skip(skip);
+        return messages;
+    }
+
 }
